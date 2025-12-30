71b2e1ca1600aa3860031d6d020c9253dea35834
MeghnaRP <96261171+MeghnaRP@users.noreply.github.com>
2025-12-30 13:36:25 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,32 @@
+package assignment1;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=-987654321;
+        System.out.println(num);
+
+        long phonenumber=9148006040789l;
+        System.out.println(phonenumber);
+
+        boolean result=true;
+        System.out.println(result);
+        
+        byte ans=127;
+        System.out.println(ans);
+        
+        short data=-32000;
+        System.out.println(data);
+        
+        char alpha='M';
+        System.out.println(alpha);
+        
+        float marks=97.2F;
+        System.out.println(marks);
+        
+        double data1=12.1234567890123;
+        System.out.println(data1);
+	}
+
+}
---
+++ Program_IfElse.java
@@ -0,0 +1,15 @@
+package assignment1;
+
+public class Program_IfElse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1=1;
+		int num2=2;
+		if(num1<num2)
+		System.out.println("smallest number is: " +num1);
+		else
+		System.out.println("smallest number is: " +num2);
+	}
+
+}
